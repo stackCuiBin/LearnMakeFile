73e29c8f838ab624299789bc8b3219e5e35a8f2d
stackCuibin <bb.cui@foxmail.com>
2020-02-27 00:28:46 +0800

    makefile结构、伪目标及变量定义的讲解

---
+++ class_02/main.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+ ******* File Name: main.c
+ ******* Author: bb.cui
+ ******* Mail: bb.cui@foxmail.com 
+ ******* Created Time: Wed 26 Feb 2020 10:47:41 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+
+extern void func(void);
+
+int main(int argc, char* argv[])
+{
+	func();
+
+    return 0;
+}
---
+++ class_03/func.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+ ******* File Name: func.c
+ ******* Author: bb.cui
+ ******* Mail: bb.cui@foxmail.com 
+ ******* Created Time: Wed 26 Feb 2020 10:45:07 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+
+void func(void)
+{
+	printf("func.c : hello makefile\n");
+
+}
